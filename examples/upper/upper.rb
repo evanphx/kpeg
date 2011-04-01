1f82400cf803e8384627a8a6be6790a57a614b64
Brian Miller <brimil01@gmail.com>
2011-03-31 23:18:00 -0700

    Fixed upper example to do string extraction correctly

--- examples/upper/upper.rb
+++ examples/upper/upper.rb
@@ -1,7 +1,7 @@
 # Make sure you have the kpeg gem installed
 require 'rubygems'
 # To generate the upper.kpeg file run kpeg upper.kpeg
-require "upper.kpeg" # Require the generated parser
+require "upper.kpeg.rb" # Require the generated parser
 
 parser = Upper.new("a lower case string. Another lower case string.")
 if parser.parse
