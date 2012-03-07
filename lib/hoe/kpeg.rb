50e051feaf7f14791735dc8365f96c05df194df2
Eric Hodel <drbrain@segment7.net>
2012-03-07 15:30:48 -0800

    Fixed hoe plugin to overwrite generated files

--- lib/hoe/kpeg.rb
+++ lib/hoe/kpeg.rb
@@ -10,7 +10,7 @@
 # license:
 #
 # Copyright (c) Ryan Davis, seattle.rb
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
 # "Software"), to deal in the Software without restriction, including
@@ -18,10 +18,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -55,9 +55,10 @@ module Hoe::Kpeg
   def initialize_kpeg
     self.kpeg_tasks = [:multi, :test, :check_manifest]
 
-    # -v = verbose
+    # -f = overwrite existing file
     # -s = parser does not require runtime
-    self.kpeg_flags ||= "-s -v"
+    # -v = verbose
+    self.kpeg_flags ||= "-s -v -f"
 
     dependency 'kpeg', '~> 0.9', :development
   end
